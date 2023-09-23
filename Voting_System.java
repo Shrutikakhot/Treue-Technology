151802d4cfcae741938a651ed3e5fcf1cbf03ac1
Shrutikakhot <101885694+Shrutikakhot@users.noreply.github.com>
2023-09-23 14:14:16 +0530

    Update Voting_System.java

--- Voting_System.java
+++ Voting_System.java
@@ -1,12 +1,14 @@
+//Code for Voting System
 import java.util.Scanner;
+//create the voting_System class
 class Voting_System
 {
     public static void main(final String[] args) 
     {
-         Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int candidate1vote=0;
         int candidate2vote=0;
-
+        //print the message in console
         System.out.println("** ** ** ** ** ** ** ** ** ** ** ");
         System.out.println(" ===Welcome to Voting_System===");
         System.out.println("** ** ** ** ** ** ** ** ** ** ** ");
@@ -19,11 +21,13 @@ class Voting_System
 
         System.out.println("\nName is:" +name);
         System.out.println("Age is:" +age);
-
+        //if age is less than 18
         if(age<18){
             System.out.println("Not are Not Eligible to vote!!");
             System.out.print("Thank You!!");
-        }else{
+        }
+        //else this will print
+        else{
             System.out.println("\nEnter the candidate name to vote for:");
             System.out.println("============================");
             System.out.println("1.candidate 1");
@@ -42,7 +46,7 @@ class Voting_System
             }else{
                 System.out.print("Invalid Candidate Selection.");
             }
-
+            //to print the vote counts
             System.out.println("\n\nThe vote count for candidate 1 is:" +candidate1vote);
             System.out.println("The vote count for candidate 2 is:" +candidate2vote);
         }  
